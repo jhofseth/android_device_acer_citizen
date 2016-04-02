2a5868e4a9946d1e771196f767f041516da82c10
jesse <jesse@cenref7002.spl.org>
2016-04-02 15:24:27 -0700

    add vendor folder, unzipped

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, device/acer/citizen/device_citizen.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := citizen
-PRODUCT_NAME := cm_citizen
+PRODUCT_NAME := a1_860_ww_gen1
 PRODUCT_BRAND := acer
-PRODUCT_MODEL := citizen
-PRODUCT_MANUFACTURER := acer
+PRODUCT_MODEL := A1-860
+PRODUCT_MANUFACTURER := Acer
--- device_citizen.mk
+++ device_citizen.mk
@@ -20,5 +20,5 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full_citizen
+PRODUCT_NAME := a1_860_ww_gen1
 PRODUCT_DEVICE := citizen
